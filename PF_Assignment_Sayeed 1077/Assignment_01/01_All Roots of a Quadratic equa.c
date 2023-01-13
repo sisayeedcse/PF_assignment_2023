9ad60cc9184b7f50c7cce68df57e46918670c9f5
aninditactg <120120477+aninditactg@users.noreply.github.com>
2023-01-13 15:34:42 +0600

    Update 01_All Roots of a Quadratic equa.c

--- PF_Assignment_Sayeed 1077/Assignment_01/01_All Roots of a Quadratic equa.c
+++ PF_Assignment_Sayeed 1077/Assignment_01/01_All Roots of a Quadratic equa.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main()
+{
+    double a, b, c, x1, x2, d;
 
-float a, b, c, d, x1, x2;
+    printf("Please enter the values of a, b and c: \n");
+    scanf("%lf %lf %lf", &a, &b, &c);
 
-printf("Please enter three values for calculation(a,b,c) : ");
-scanf("%f%f%f", &a, &b, &c);
+    d = (b * b) - 4 * a * c;
 
-d = (b*b)- 4*a*c;
+    if (d > 0){
+        x1 = (- b + sqrt (d)) / (2 * a);
+        x2 = (- b - sqrt (d)) / (2 * a);
+        printf("The real roots = %.2lf %.2lf", x1, x2);
+    }
+    else if(d == 0){
+        x1 = - b / (2 * a);
+        x2 = - b / (2 * a);
+        printf("Roots are equal = %.2lf %.2lf", x1, x2);
+    }
+    else {
+        printf("Roots are imaginary");
+    }
 
-if(d > 0){
-    x1 = -b + sqrt(d) / 2*a;
-    x1 = -b - sqrt(d) / 2*a;
-    printf("These are your results- %f %f", x1, x2);
-}
-else if( d == 0){
-    x1 = -b/2*a;
-    printf("This is your result of Quadratic equation- %f",x1);
-}
-else printf("Please enter right values!");
+    return 0;
 
-return 0;
 }
